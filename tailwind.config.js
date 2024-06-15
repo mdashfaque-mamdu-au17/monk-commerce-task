7b8860400c1d5b6d3ae9bfd25806082c6f24a8b2
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2024-06-16 01:59:47 +0530

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,11 @@ export default {
       colors: {
         'secondary-black': '#222222',
         'black-500': '#303030',
+        'light-gray': '#F5F7FB',
         'primary-gray': '#8E8E93',
         'light-yellow': '#DCF7C5',
         'cream-white': '#FAFAFA',
+        'primary-green': '#3BA55D',
       },
     },
   },
