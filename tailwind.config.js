0aaf5eea95aec39b0255fbfa0b41306e8b71f235
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2024-06-15 17:35:57 +0530

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'secondary-black': '#222222',
+        'black-500': '#303030',
+        'primary-gray': '#8E8E93',
+      },
+    },
   },
   plugins: [],
 };
