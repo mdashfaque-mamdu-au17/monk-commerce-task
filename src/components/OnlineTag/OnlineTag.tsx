9881d427f695d571ffbcccd1b0798fc47445cd02
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2024-06-16 02:00:59 +0530

    feat: online tag component created

---
+++ src/components/OnlineTag/OnlineTag.tsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames';
+
+function OnlineTag() {
+  return (
+    <div
+      className={classNames('w-3 h-3 rounded-full bg-primary-green')}
+    ></div>
+  );
+}
+
+export default OnlineTag;
