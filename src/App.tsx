8fa8cd2c4f4e6de5fdf93b039f37fc0cb311a486
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2024-06-15 17:36:34 +0530

    component texting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Heading, Text } from './components';
+
 function App() {
   return (
     <>
       <div className="bg-green-300">Hello world</div>
+      <Heading headingType="h1">Chats</Heading>
+      <Heading headingType="h3" variant="medium">
+        Josh California
+      </Heading>
+
+      <div className="mt-10">
+        <Text variant="gray">I think top two are:</Text>
+        <Text variant="black-one">Mark as unread</Text>
+        <Text variant="black-two">Click here for contact info</Text>
+        <Text variant="black-three">
+          Morning! It was pretty good. Went hiking on Saturday. You?
+        </Text>
+      </div>
     </>
   );
 }
