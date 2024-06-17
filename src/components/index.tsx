061201e67901cc2640298a35324abd507875cc54
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2024-06-17 18:20:44 +0530

    component added

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import Text from './Texts/Text';
 import MessageBox from './MessageBox/MessageBox';
 import Profile from './Profile/Profile';
 import Button from './Button/Button';
+import InputField from './InputField/InputField';
 
-export { Heading, Text, MessageBox, Profile, Button };
+export { Heading, Text, MessageBox, Profile, Button, InputField };
